9138d00550f8bcd612b3750d95e44ebb9b276836
estrellafg <estrellafergi@gmail.com>
2020-10-27 16:24:31 +0100

    fit ellipsoid method

--- tomo/utils.py
+++ tomo/utils.py
@@ -39,8 +39,10 @@ def _getUniqueFileName(pattern, filename, filePaths=None):
  commPath = pwutils.commonPath(filePaths)
  return filename.replace(commPath + "/", "").replace("/", "_")
 
+
 def _matchFileNames(originalName, importName):
  return os.path.basename(importName) in originalName
+
 
 def rotation_matrix_from_vectors(vec1, vec2):
     # a, b = (vec1 / np.linalg.norm(vec1)).reshape(3), (vec2 / np.linalg.norm(vec2)).reshape(3)
@@ -58,12 +60,88 @@ def rotation_matrix_from_vectors(vec1, vec2):
     else:
         return np.eye(4)
 
+
 def delaunayTriangulation(cloud):
     cloud = pv.PolyData(cloud)
     mesh = cloud.delaunay_3d()
     shell = mesh.extract_geometry().triangulate()
     return shell
 
+
 def computeNormals(triangulation):
     triangulation.compute_normals(inplace=True)
     return triangulation.point_normals
+
+
+def fit_ellipsoid(x, y, z):
+    """ Fit ellipsoid in the form Ax^2 + By^2 + Cz^2 + 2Dxy + 2Exz + 2Fyz + 2Gx + 2Hy + 2Iz + J = 0
+    and A + B + C = 3 constraint removing one extra parameter (from Matlab function "Fit Ellipsoid"). """
+
+    # OUTPUT:
+    # center: ellispoid center coordinates[xc, yc, zc]
+    # radii: ellipsoid radii[a, b, c]
+    # evecs: the radii directions as columns of the 3x3 matrix
+    # v: the 10 parameters describing the ellipsoid algebraically:
+    #     Ax^2 + By^2 + Cz^2 + 2Dxy + 2Exz + 2Fyz + 2Gx + 2Hy + 2Iz + J = 0
+    # chi2: residual sum of squared errors(chi^2), in the coordinate frame in which the ellipsoid is a unit sphere
+
+    D = np.array([x*x + y*y - 2*z*z, x*x + z*z - 2*y*y, 2*x*y, 2*x*z, 2*y*z, 2*x, 2*y, 2*z, 1 + 0*x])  # fxc = 9x10
+    D = D.transpose()  # fxc = 10x9
+
+    # Solve the normal system of equations
+    d2 = x*x + y*y + z*z  # The RHS of the llsq problem (y's) ; fxc = 1x10
+    cD = D.conj().transpose()  # fxc = 9x10
+    a = cD @ D  # fxc = 9x9
+    b = cD @ d2  # fxc = 9x1
+    u = np.linalg.lstsq(a, b, rcond=None)[0]  # Solution to the normal equations ; fxc = 9x1
+
+    # print("-----D----", D)   # fxc = 10x9
+    # print("-----d2----", d2) # fxc = 1x10
+    # print("-----cD----", cD) # fxc = 9x10
+    # print("-----a----", a)   # fxc = 9x9
+    # print("-----b----", b)   # fxc = 9x1
+    # print("-----u----", u)   # fxc = 9x1
+
+    # Find the ellipsoid parameters
+    # Convert back to the conventional algebraic form
+    v = np.zeros(10)
+    v[0] = u[0] + u[1] - 1
+    v[1] = u[0] - 2 * u[1] - 1
+    v[2] = u[1] - 2 * u[0] - 1
+    v[3:10] = u[2:9]
+
+    # Form the algebraic form of the ellipsoid
+    A = np.array([[v[0], v[3], v[4], v[6]],
+                  [v[3], v[1], v[5], v[7]],
+                  [v[4], v[5], v[2], v[8]],
+                  [v[6], v[7], v[8], v[9]]])
+
+    # Find the center of the ellipsoid
+    center = np.linalg.lstsq(-A[0:3, 0:3], v[6:9], rcond=None)[0]
+
+    # Form the corresponding translation matrix
+    T = np.eye(4)
+    T[3, 0:3] = center.conj().transpose()
+
+    # Translate to the center
+    R = T * A * T.conj().transpose()
+
+    # Solve the eigenproblem
+    [evals, evecs] = np.linalg.eig(R[0:3, 0:3] / -R[3, 3])
+    radii = np.sqrt(1/abs(evals))
+    sgns = np.sign(evals)
+    radii = radii * sgns
+
+    # Calculate difference of the fitted points from the actual data normalized by the conic radii
+    d = np.array([x - center[0], y - center[1], z - center[2]])  # shift data to origin
+    d = d.transpose() @ evecs  # Rotate to cardinal axes of the conic
+    d = d.transpose()
+    d = np.array([d[:, 0] / radii[0], d[:, 1] / radii[1], d[:, 2] / radii[2]])  # normalize to the conic radii
+    chi2 = np.sum(np.abs(1 - np.sum(np.dot((d**2), np.tile(sgns.conj().transpose(), (d.shape[0], 1)).transpose()), 1)))
+
+    if np.abs(v[-1]) > 1e-6:
+        v = -v / v[-1]  # Normalize to the more conventional form with constant term = -1
+    else:
+        v = -np.sign(v[-1]) * v
+
+    return center, radii, v, evecs, chi2
