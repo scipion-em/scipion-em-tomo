072cdeac1ea8c6515f419fe48c65abbcf37a7e31
DavidHerreros <david.herreroscalero@gmail.com>
2021-02-25 13:54:29 +0100

    protocol_alignment_assign_subtomo modified to work with Coordinates3D and SubTomograms

--- tomo/protocols/protocol_alignment_assign_subtomo.py
+++ tomo/protocols/protocol_alignment_assign_subtomo.py
@@ -1,6 +1,7 @@
 # **************************************************************************
 # *
 # * Authors:     Estrella Fernandez Gimenez (me.fernandez@cnb.csic.es)
+# *              David Herreros Calero (dherreros@cnb.csic.es) (Coordinates3D compatibility)
 # *
 # * Unidad de  Bioinformatica of Centro Nacional de Biotecnologia , CSIC
 # *
@@ -27,19 +28,24 @@
 from pyworkflow.protocol.params import PointerParam
 from pwem.protocols import EMProtocol
 
+import tomo.objects as tomoObjs
+
 class ProtAlignmentAssignSubtomo(EMProtocol):
-    """ Assign the alignment calculated for a set of subtomograms to another set.
+    """ Assign the alignment stored in a set of Subtomograms/Coordinates3D
+    to another set.
     Both sets should have same pixel size (A/px).
-    The subtomograms with the alignment can also be a subset of a bigger set.
+    The Subtomograms/Coordinates3D with the alignment can also be a subset of a bigger set.
     """
-    _label = 'assign alignment subtomo'
+    _label = 'assign alignment'
 
     def _defineParams(self, form):
         form.addSection(label='Input')
-        form.addParam('inputSubtomos', PointerParam, pointerClass='SetOfSubTomograms', label='Subtomograms',
-                      help='Select the subtomograms that you want to update the new alignment.')
-        form.addParam('inputAlignment', PointerParam, pointerClass='SetOfSubTomograms', label="Aligned subtomograms",
-                      help='Select the subtomograms with alignment to be apply to the other particles.')
+        form.addParam('input', PointerParam, pointerClass='SetOfSubTomograms, SetOfCoordinates3D',
+                      label='Input',
+                      help='Select the Subtomograms/Coordinates3D that you want to update the new alignment.')
+        form.addParam('inputAlignment', PointerParam, pointerClass='SetOfSubTomograms, SetOfCoordinates3D',
+                      label="Alignments",
+                      help='Select the Subtomograms/Coordinates3D with alignment to be apply to the other object.')
 
     # --------------------------- INSERT steps functions --------------------------
     def _insertAllSteps(self):
@@ -47,52 +53,66 @@ class ProtAlignmentAssignSubtomo(EMProtocol):
 
     # --------------------------- STEPS functions --------------------------------------------
     def createOutputStep(self):
-        inputSubtomos = self.inputSubtomos.get()
+        input = self.input.get()
         inputAlignment = self.inputAlignment.get()
-        outputSubtomos = inputSubtomos.create(self._getPath())
-        outputSubtomos.copyInfo(inputSubtomos)
-        outputSubtomos.setAlignment(inputAlignment.getAlignment())
-        outputSubtomos.copyItems(inputSubtomos, updateItemCallback=self._updateItem)
-        self._defineOutputs(outputSubtomograms=outputSubtomos)
-        self._defineSourceRelation(self.inputSubtomos, outputSubtomos)
-        self._defineSourceRelation(self.inputAlignment, outputSubtomos)
+        output = input.create(self._getPath())
+        output.copyInfo(input)
+        if isinstance(output, tomoObjs.SetOfSubTomograms):
+            output.setAlignment(inputAlignment.getAlignment())
+        output.copyItems(input, updateItemCallback=self._updateItem)
+        self._defineOutputs(outputAligned=output)
+        self._defineSourceRelation(self.input, output)
+        self._defineSourceRelation(self.inputAlignment, output)
 
     # --------------------------- UTILS functions --------------------------------------------
     def _updateItem(self, item, row):
         # Add alignment info from corresponding item on inputAlignment
         inputAlignment = self.inputAlignment.get()
-        alignedParticle = inputAlignment[item.getObjId()]
-        # If alignment is found for this particle set the alignment info
-        # on the output particle, if not do not write that item
-        if alignedParticle is not None:
-            alignment = alignedParticle.getTransform()
-            item.setTransform(alignment)
+        alignedObj = inputAlignment[item.getObjId()]
+        # If alignment is found for this object set the alignment info
+        # on the output object, if not do not write that item
+        if alignedObj is not None:
+            alignment = self._getAlignment(alignedObj)
+            self._setAlignment(item, alignment)
         else:
             item._appendItem = False
+
+    def _getAlignment(self, object):
+        if isinstance(object, tomoObjs.Coordinate3D):
+            return object.getMatrix()
+        elif isinstance(object, tomoObjs.SubTomogram):
+            return object.getTransform()
+
+    def _setAlignment(self, object, alignment):
+        if isinstance(object, tomoObjs.Coordinate3D):
+            object.setMatrix(alignment)
+        elif isinstance(object, tomoObjs.SubTomogram):
+            return object.setTransform(alignment)
 
     # --------------------------- INFO functions --------------------------------------------
     def _summary(self):
         summary = []
-        if not hasattr(self, 'outputSubtomograms'):
-            summary.append("Output subtomograms not ready yet.")
+        if not hasattr(self, 'outputAligned'):
+            summary.append("Output not ready yet.")
         else:
-            summary.append("Assigned alignment to %s subtomograms from a total of %s." % (
-                self.outputSubtomograms.getSize(), self.inputSubtomos.get().getSize()))
+            summary.append("Assigned alignment to %s objects from a total of %s." % (
+                self.outputAligned.getSize(), self.input.get().getSize()))
         return summary
 
     def _methods(self):
         methods = []
-        if not hasattr(self, 'outputSubtomograms'):
-            methods.append("Output subtomograms not ready yet.")
+        if not hasattr(self, 'outputAligned'):
+            methods.append("Output not ready yet.")
         else:
-            methods.append("We assigned alignment to %s subtomograms from %s and produced %s."
-                           % (self.outputSubtomograms.getSize(), self.getObjectTag('inputSubtomos'),
-                              self.getObjectTag('outputSubtomograms')))
+            methods.append("We assigned alignment to %s objects from %s and produced %s."
+                           % (self.outputAligned.getSize(), self.getObjectTag('input'),
+                              self.getObjectTag('outputAligned')))
         return methods
 
     def _validate(self):
         validateMsgs = []
-        for subtomo in self.inputAlignment.get().iterItems():
-            if not subtomo.hasTransform():
-                validateMsgs.append('Please provide subtomograms which have transformation matrix in "inputAlignment".')
+        for input in self.inputAlignment.get().iterItems():
+            if not input.hasTransform():
+                validateMsgs.append('Please provide Subtomograms/Coordinates3D which '
+                                    'have transformation matrix in "inputAlignment".')
         return validateMsgs
