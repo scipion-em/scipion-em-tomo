45a33b3903e5dc7c999d20d5d27f59007780915f
fede-pe <fepe.ig@gmail.com>
2021-08-30 11:40:26 +0200

    new protocol to detect residual misali

---
+++ tomo/protocols/protocol_residual_misalignment_detection.py
@@ -0,0 +1,38 @@
+# **************************************************************************
+# *
+# * Authors:     Federico P. de Isidro Gomez (fp.deisidro@cnb.csic.es) [1]
+# *
+# * [1] Centro Nacional de Biotecnologia, CSIC, Spain
+# *
+# * This program is free software; you can redistribute it and/or modify
+# * it under the terms of the GNU General Public License as published by
+# * the Free Software Foundation; either version 2 of the License, or
+# * (at your option) any later version.
+# *
+# * This program is distributed in the hope that it will be useful,
+# * but WITHOUT ANY WARRANTY; without even the implied warranty of
+# * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# * GNU General Public License for more details.
+# *
+# * You should have received a copy of the GNU General Public License
+# * along with this program; if not, write to the Free Software
+# * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+# * 02111-1307  USA
+# *
+# *  All comments concerning this program package may be sent to the
+# *  e-mail address 'scipion@cnb.csic.es'
+# *
+# **************************************************************************
+
+from pyworkflow import BETA
+from pwem.protocols import EMProtocol
+from tomo.protocols import ProtTomoBase
+
+
+class ProtResidualMisalignmentDetection(EMProtocol, ProtTomoBase):
+    """
+    Detection of misalignment in a set of tilt-series according to the obtained fiducial residuals.
+    """
+
+    _label = 'Residual misalignment detection'
+    _devStatus = BETA
