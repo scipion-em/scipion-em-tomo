3d50517d1996a7175418de47820144d423ecb4c8
fede-pe <fepe.ig@gmail.com>
2022-06-14 17:47:24 +0200

    first structure + form

---
+++ tomo/protocols/protocol_ts_convert_coords3d.py
@@ -0,0 +1,79 @@
+# **************************************************************************
+# *
+# * Authors:     Federico P. de Isidro Gomez (fp.deisidro@cnb.csi.es) [1]
+# *
+# * [1] Centro Nacional de Biotecnologia, CSIC, Spain
+# *
+# * This program is free software; you can redistribute it and/or modify
+# * it under the terms of the GNU General Public License as published by
+# * the Free Software Foundation; either version 2 of the License, or
+# * (at your option) any later version.
+# *
+# * This program is distributed in the hope that it will be useful,
+# * but WITHOUT ANY WARRANTY; without even the implied warranty of
+# * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# * GNU General Public License for more details.
+# *
+# * You should have received a copy of the GNU General Public License
+# * along with this program; if not, write to the Free Software
+# * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+# * 02111-1307  USA
+# *
+# *  All comments concerning this program package may be sent to the
+# *  e-mail address 'scipion@cnb.csic.es'
+# *
+# **************************************************************************
+
+import os
+from pyworkflow import BETA
+import pyworkflow.protocol.params as params
+import pyworkflow.utils.path as path
+from pyworkflow.object import Set, List, String
+from pyworkflow.protocol.constants import STEPS_PARALLEL
+from pwem.protocols import EMProtocol
+import tomo.objects as tomoObj
+from tomo.protocols import ProtTomoBase
+import xmipptomo.utils as utils
+
+METADATA_INPUT_COORDINATES = "fiducialCoordinates.xmd"
+
+
+class XmippProtTsConvertCoords3d(EMProtocol, ProtTomoBase):
+    """
+    Scipion protocol to convert a set of tilt-series coordinates 3d to a set of coordinates 3d associated to a set of
+    tomograms.
+    """
+
+    _label = 'detect misaligned TS'
+    _devStatus = BETA
+
+    def __init__(self, **kwargs):
+        EMProtocol.__init__(self, **kwargs)
+        ProtTomoBase.__init__(self)
+
+    # -------------------------- DEFINE param functions -----------------------
+    def _defineParams(self, form):
+        form.addSection('Input')
+
+        form.addParam('inputSetOfCoordinates',
+                      params.PointerParam,
+                      pointerClass='SetOfTiltSeriesCoordinates',
+                      important=True,
+                      label='Input set of coordinates 3D',
+                      help='Set of 3D coordinates indicating the position in space of the fiducials. This set should '
+                           'be obtained from the previous alignment step of the tilt-series.')
+
+        form.addParam('inputSetOfTomograms',
+                      params.PointerParam,
+                      pointerClass='SetOfTomograms',
+                      important=True,
+                      label='Input set of tomograms')
+
+    # -------------------------- INSERT steps functions ---------------------
+    def _insertAllSteps(self):
+
+        for ts in self.inputSetOfTiltSeries.get():
+            self._insertFunctionStep(self.convertCoordinates)
+
+    # --------------------------- STEPS functions ----------------------------
+
