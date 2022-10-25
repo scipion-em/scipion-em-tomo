36bed314d64fc5d6742afe7988d7b35b13a319c9
Yunior C. Fonseca Reyna <cfonseca@cnb.csic.es>
2022-10-25 10:19:59 +0200

    * Modifying the ctf validate protocol    - now, three criteria are considered * The ctf viewer is based on the "enabled" parameter * Removed from CTFTomo object the ctfs validation attributes

--- tomo/protocols/protocol_ctf_validate.py
+++ tomo/protocols/protocol_ctf_validate.py
@@ -24,12 +24,17 @@
 # *  e-mail address 'scipion@cnb.csic.es'
 # *
 # **************************************************************************
-import os
-
+import logging
 import pyworkflow.protocol.params as params
 from pwem.protocols import EMProtocol
 from pyworkflow import BETA
+import pyworkflow.object as pwobj
+
 from tomo.objects import SetOfCTFTomoSeries
+logger = logging.getLogger(__name__)
+
+OUTPUT_BAD_CTF_SERIE = "badCTFTomoSeries"
+OUTPUT_GOOD_CTF_SERIE = "goodCTFTomoSeries"
 
 
 class ProtCTFTomoSeriesValidate(EMProtocol):
@@ -47,99 +52,276 @@ class ProtCTFTomoSeriesValidate(EMProtocol):
         form.addParam('inputCtfTomoSeries', params.PointerParam, important=True,
                       pointerClass='SetOfCTFTomoSeries',
                       label='Input ctf tomo series')
-        form.addParam('criteria',  params.EnumParam,
-                      choices=["defocus tolerance"], label="Criteria",
-                      default=0, help="Criteria to validate: \n\n"
-                                      "defocus tolerance: Calculate the defocus deviation taking into account a threshold(percent tolerance) respect to the mean.")
-        form.addParam('tolerance', params.FloatParam, label='Tolerance percent',
-                      condition='criteria==0', default=20,
-                      help="Percent Tolerance")
+
+        form.addSection(label='CTF validation')
+        form.addParam('validationType', params.EnumParam,
+                      display=params.EnumParam.DISPLAY_HLIST,
+                      choices=["Global", "Per tilt"],
+                      label="Validation type",
+                      default=0,
+                      help="Global mode: the series with at least "
+                           "one image that does not satisfy the criteria are "
+                           "rejected \n"
+                           "Per tilt mode: the series containing a certain "
+                           "number of images that does not satisfy the "
+                           "criteria are rejected")
+        form.addParam('numberImages', params.IntParam,
+                      label='Number of images to rejected',
+                      condition='validationType==1', default=None,
+                      allowsNull=True,
+                      help="Number of images taking into account to rejected a "
+                           "ctf series")
+        self.addCriteriaParams(form)
+
+    def addCriteriaParams(self, form):
+        form.addParam('defocusCriteria', params.EnumParam,
+                      display=params.EnumParam.DISPLAY_HLIST,
+                      choices=["Yes", "No"],
+                      label="Defocus tolerance",
+                      default=1, help="Validate the defocus deviation taking "
+                                      "into account a threshold(tolerance) "
+                                      "respect to a defocus expected value.")
+        form.addParam('defocusValue', params.FloatParam,
+                      label='Expected value (Å)',
+                      condition='defocusCriteria==0', default=None,
+                      allowsNull=True,
+                      help="Defocus expected value in Å")
+        form.addParam('defocusTolerance', params.FloatParam,
+                      label='Tolerance value (Å)',
+                      condition='defocusCriteria==0', default=None,
+                      allowsNull=True,
+                      help="Defocus tolerance value in Å")
+
+        form.addParam('astigmatismCriteria', params.EnumParam,
+                      display=params.EnumParam.DISPLAY_HLIST,
+                      choices=["Yes", "No"],
+                      label="Astigmatism",
+                      default=1, help="Validate the astigmatism taking into "
+                                      "account a tolerance value.")
+        form.addParam('astigmatismTolerance', params.FloatParam,
+                      label='Tolerance value',
+                      condition='astigmatismCriteria==0', default=1.1,
+                      help="Astigmatism tolerance value")
+
+        form.addParam('resolutionCriteria', params.EnumParam,
+                      display=params.EnumParam.DISPLAY_HLIST,
+                      choices=["Yes", "No"],
+                      label="Resolution",
+                      default=1, help="Validate the resolution taking into "
+                                      "account a expected resolution.")
+        form.addParam('resolutionTolerance', params.FloatParam,
+                      label='Expected value',
+                      allowsNull=True,
+                      condition='resolutionCriteria==0', default=None,
+                      help="Expected resolution value")
 
     def _insertAllSteps(self):
         self._insertFunctionStep(self.ctfValidateStep)
         self._insertFunctionStep(self.createOutputStep)
 
+    # ----------------------------STEPS --------------------------------------
     def ctfValidateStep(self):
         """
         Validate all ctf tomo series and separate into two sets(good and bad
         following the selected criteria)
         """
+
+        self.goodCTFTomoSeries = None
+        self.badCTFTomoSeries = None
+        self.validateDict = {}
+        imagesToReject = 1  # Case of global validation type
         ctfSeries = self.inputCtfTomoSeries.get()
-        self.goodCtfName = 'goodSetOfCTFTomoSeries'
-        self.badCtfName = 'badSetOfCTFTomoSeries'
-        self.outputCtfName = 'outputSetOfCTFTomoSeries'
 
-        self.outputSetOfCtfTomoSeries = SetOfCTFTomoSeries.create(self._getPath(),
-                                                                  template='CTFmodels%s.sqlite')
-        self.outputSetOfCtfTomoSeries.setSetOfTiltSeries(ctfSeries.getSetOfTiltSeries(pointer=True).get())
+        if self.validationType.get() == 1:
+            imagesToReject = self.numberImages.get()
 
-        if self.criteria.get() == 0:  # Defocus angles case
-            self._validateCtfDefocusDeviation(ctfSeries, self.tolerance.get())
+        self.printTable(imagesToReject)
+        printStr = ""
+        for ctfSerie in ctfSeries:
+            newCTFTomoSeries = ctfSerie.clone()
+            ctfEstItems = []
+            faileDefocusCriteria = 0
+            failedAstigmatismCriteria = 0
+            failedResolutionCriteria = 0
+            for item in ctfSerie.iterItems():
+                ctfEstItem = item.clone()
+                ctfEstItems.append(ctfEstItem)
 
-    def _validateCtfDefocusDeviation(self, ctfSeries, tolerance):
+            # Defocus angles criteria
+            if self.defocusCriteria.get() == 0:
+                faileDefocusCriteria = self._validateCtfDefocusDeviation(ctfEstItems,
+                                                                         self.defocusValue.get(),
+                                                                         self.defocusTolerance.get())
+
+            # Astigmatism criteria
+            if self.astigmatismCriteria.get() == 0:
+                failedAstigmatismCriteria = self._validateAstigmatism(ctfEstItems,
+                                                                      self.astigmatismTolerance.get())
+
+            # Resolution criteria
+            if self.resolutionCriteria.get() == 0:
+                # Some estimation methods don't calculate the resolution
+                if ctfEstItems[0].getResolution() is not None:
+                    self._validateResolution(ctfEstItems,
+                                             self.resolutionTolerance.get())
+
+            validate = self.validateCtf(ctfEstItems,
+                                        imagesToReject=imagesToReject)
+
+            result = "Pass" if validate else "Rejected"
+            printStr += "{0:25} {1:30} {2:30} {3:30} {4:^50}".format(ctfSerie.getTsId(),
+                                                             faileDefocusCriteria,
+                                                             failedAstigmatismCriteria,
+                                                             failedResolutionCriteria,
+                                                             result)
+            printStr += "\n"
+
+            if validate:
+                newCTFTomoSeries.setEnabled(True)
+                output = self.getOutputSetOfCTFTomoSeries(OUTPUT_GOOD_CTF_SERIE)
+                output.append(newCTFTomoSeries)
+            else:
+                newCTFTomoSeries.setEnabled(False)
+                output = self.getOutputSetOfCTFTomoSeries(OUTPUT_BAD_CTF_SERIE)
+                output.append(newCTFTomoSeries)
+
+            for ctfItem in ctfEstItems:
+                newCTFTomoSeries.append(ctfItem)
+
+        logger.info(printStr)
+
+    def createOutputStep(self):
+        if self.goodCTFTomoSeries is not None:
+            self.goodCTFTomoSeries.setStreamState(pwobj.Set.STREAM_CLOSED)
+            self.goodCTFTomoSeries.write()
+            self._store()
+        if self.badCTFTomoSeries is not None:
+            self.badCTFTomoSeries.setStreamState(pwobj.Set.STREAM_CLOSED)
+            self.badCTFTomoSeries.write()
+            self._store()
+
+    # --------------------------UTILS methods ----------------------------------
+    def _validateCtfDefocusDeviation(self, ctfEstItems, defocusValue, tolerance):
         """
         Validate the set of ctf tomo series taking into account de defocus angle
         deviation
         """
-        # Creating a new copy of ctf tomo series and recalculate the defocus
-        # deviation
-        self.outputSetOfgoodCtfTomoSeries = None
-        self.outputSetOfbadCtfTomoSeries = None
+        failedCTF = self.validateDefocusUDeviation(ctfEstItems, defocusValue,
+                                                   defocusUTolerance=tolerance)
+        failedCTF += self.validateDefocusVDeviation(ctfEstItems, defocusValue,
+                                                    defocusVTolerance=tolerance)
+        return failedCTF
 
-        for ctfSerie in ctfSeries:
-            newCTFTomoSeries = ctfSerie.clone()
-            newCTFTomoSeries._isDefocusUDeviationInRange.set(True)
-            newCTFTomoSeries._isDefocusVDeviationInRange.set(True)
-            self.outputSetOfCtfTomoSeries.append(newCTFTomoSeries)
-            for item in ctfSerie.iterItems():
-                ctfEstItem = item.clone()
-                newCTFTomoSeries.append(ctfEstItem)
-            newCTFTomoSeries.calculateDefocusUDeviation(defocusUTolerance=tolerance)
-            newCTFTomoSeries.calculateDefocusVDeviation(defocusVTolerance=tolerance)
-            newCTFTomoSeries.write()
-            self.outputSetOfCtfTomoSeries.update(newCTFTomoSeries)
+    def _validateAstigmatism(self, ctfEstItems, astigmatismTolerance):
+        """
+         Validate the set of ctf tomo series taking into account de astigmatism
+         threshold:
+         the _objEnable property is set as True if the astigmatism is in range or False
+         in other case, in each ctfTomoItems
+         """
+        failedCTF = 0
+        for ctfTomo in ctfEstItems:
+            isAstigmatismInRange = True if ctfTomo.getDefocusAngle() < astigmatismTolerance else False
+            if not isAstigmatismInRange:
+                failedCTF += 1
+                ctfTomo.setEnabled(isAstigmatismInRange)
+        return failedCTF
 
-        for ctfSerie in self.outputSetOfCtfTomoSeries:
-            ctfSerieClon = ctfSerie.clone()
-            # Store good and bad ctf series
-            if ctfSerie.getIsDefocusUDeviationInRange():
-                if self.outputSetOfgoodCtfTomoSeries is None:
-                    self.outputSetOfgoodCtfTomoSeries = self.outputSetOfCtfTomoSeries.createCopy(
-                        self._getExtraPath(),
-                        prefix=self.goodCtfName,
-                        copyInfo=True)
-                else:
-                    self.outputSetOfgoodCtfTomoSeries.enableAppend()
-                self.outputSetOfgoodCtfTomoSeries.append(ctfSerieClon)
-            else:
-                if self.outputSetOfbadCtfTomoSeries is None:
-                    self.outputSetOfbadCtfTomoSeries = self.outputSetOfCtfTomoSeries.createCopy(
-                        self._getExtraPath(),
-                        prefix=self.badCtfName,
-                        copyInfo=True)
-                else:
-                    self.outputSetOfbadCtfTomoSeries.enableAppend()
-                self.outputSetOfbadCtfTomoSeries.append(ctfSerieClon)
+    def _validateResolution(self, ctfEstItems, resolutionTolerance):
+        failedCTF = 0
+        for ctfTomo in ctfEstItems:
+            isAstigmatismInRange = True if ctfTomo.getResolution() < resolutionTolerance else False
+            if not isAstigmatismInRange:
+                failedCTF += 1
+                ctfTomo.setEnabled(isAstigmatismInRange)
+        return failedCTF
 
-            for item in ctfSerie.iterItems():
-                ctfEstItem = item.clone()
-                ctfSerieClon.append(ctfEstItem)
+    @staticmethod
+    def validateCtf(ctfItemsList, imagesToReject=1):
+        """
+         Validate the ctftomo serie.
+         Return the number of images marked as disable
+        """
+        count = 0
+        for ctfItem in ctfItemsList:
+            if not ctfItem.isEnabled():
+                count += 1
+            if count == imagesToReject:
+                return False
+        return True
 
-    def createOutputStep(self):
-        if self.outputSetOfgoodCtfTomoSeries is not None:
-            self._defineOutputs(**{self.goodCtfName: self.outputSetOfgoodCtfTomoSeries})
-        if self.outputSetOfbadCtfTomoSeries is not None:
-            self._defineOutputs(**{self.badCtfName: self.outputSetOfbadCtfTomoSeries})
+    def validateDefocusVDeviation(self, ctfTomoItems, defocusVValue, defocusVTolerance=20):
+        """
+          Set _objEnable property as True if the deviation is in range or False
+          in other case, in each ctfTomoItems
+        """
+        failedCTF = 0
+        for ctfTomo in ctfTomoItems:
+            defocusVdeviation = abs(ctfTomo.getDefocusV() - defocusVValue)
+            isDefocusVDeviationInRange = True if defocusVdeviation < defocusVTolerance else False
+            if not isDefocusVDeviationInRange and ctfTomo.isEnabled():
+                failedCTF += 1
+                ctfTomo.setEnabled(isDefocusVDeviationInRange)
+        return failedCTF
+
+    def validateDefocusUDeviation(self, ctfTomoItems, defocusUValue, defocusUTolerance=20):
+        """
+        Set _objEnable property as True if the deviation is in range or False
+        in other case, in each ctfTomoItems
+        """
+        failedCTF = 0
+        for ctfTomo in ctfTomoItems:
+            defocusUdeviation = abs(ctfTomo.getDefocusU() - defocusUValue)
+            isDefocusUDeviationInRange = True if defocusUdeviation < defocusUTolerance else False
+            if not isDefocusUDeviationInRange and ctfTomo.isEnabled():
+                failedCTF += 1
+                ctfTomo.setEnabled(isDefocusUDeviationInRange)
+        return failedCTF
+
+
+    def _getSetOfTiltSeries(self, pointer=False):
+        return self.inputCtfTomoSeries.get().getSetOfTiltSeries(pointer=pointer)
+
+    def getOutputSetOfCTFTomoSeries(self, outputSetName):
+        outputSetOfCTFTomoSeries = getattr(self, outputSetName, None)
+
+        if outputSetOfCTFTomoSeries:
+            outputSetOfCTFTomoSeries.enableAppend()
+        else:
+            outputSetOfCTFTomoSeries = SetOfCTFTomoSeries.create(self._getPath(),
+                                                                 prefix=outputSetName)
+            outputSetOfCTFTomoSeries.setSetOfTiltSeries(self._getSetOfTiltSeries())
+            outputSetOfCTFTomoSeries.setStreamState(pwobj.Set.STREAM_OPEN)
+            self._defineOutputs(**{outputSetName: outputSetOfCTFTomoSeries})
+
+        return outputSetOfCTFTomoSeries
 
     def allowsDelete(self, obj):
         return True
 
+    def _validate(self):
+        return []
+
     def _summary(self):
         summary = []
-        if hasattr(self, 'goodSetOfCTFTomoSeries'):
-            summary.append("Number of good ctf series: %d." % (self.goodSetOfCTFTomoSeries.getSize()))
-        if hasattr(self, 'badSetOfCTFTomoSeries'):
-            summary.append("Number of bad ctf series: %d." % (self.badSetOfCTFTomoSeries.getSize()))
+        if hasattr(self, 'goodCTFTomoSeries'):
+            summary.append("Number of good ctf series: %d." % (self.goodCTFTomoSeries.getSize()))
+        if hasattr(self, 'badCTFTomoSeries'):
+            summary.append("Number of bad ctf series: %d." % (self.badCTFTomoSeries.getSize()))
 
         return summary
+
+    def printTable(self, imagesToReject):
+        printStr = "\n-------------------------------------------------------\n"
+        printStr += "Number of failed images to reject: %s\n" % imagesToReject
+        printStr += '--------------------------------------------------------\n'
+        printStr += "{0:35} {1:25} {2:25} {3:25} {4:20}".format('CTFTomoSerie',
+                                                         'Defocus tolerance',
+                                                         'Astigmatism',
+                                                         'Resolution',
+                                                         'Validate').ljust(5, ' ')
+        printStr += "\n"
+        printStr += '------------------------' * 7
+
+        logger.info(printStr)
 
