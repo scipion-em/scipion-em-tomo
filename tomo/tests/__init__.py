533067544e70b97b534133945e6f78028056ca2a
JorMaister <jorgejjs86@gmail.com>
2024-05-27 16:37:12 +0200

    Update relion test dataset desc

--- tomo/tests/__init__.py
+++ tomo/tests/__init__.py
@@ -190,6 +190,7 @@ class DataSetRe4STATuto(Enum):
     maskBin4 = 'masks/mask_align_bin4.mrc'
     maskBin2 = 'masks/mask_align_bin2.mrc'
     maskFscBin2 = 'masks/mask_fsc_bin2.mrc'
+    maskFscBin4 = 'masks/mask_fsc_bin4.mrc'
     # For EMAN testing
     initVolByEman = 'testEman/initialModel0ByEman.mrc'
     # For CISTEM Ctffind import testing
