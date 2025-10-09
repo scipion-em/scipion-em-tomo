1f3e783697d2c0135b009f2f3747ed91d2d69dbf
JorMaister <jorgejjs86@gmail.com>
2025-10-09 15:11:40 +0200

    Clean unused import

--- tomo/tests/test_base_centralized_layer.py
+++ tomo/tests/test_base_centralized_layer.py
@@ -24,7 +24,7 @@
 # *
 # **************************************************************************
 from os.path import exists, isabs, islink
-from typing import List, Union, Tuple, Optional
+from typing import List, Union, Optional
 
 import mrcfile
 import numpy as np
