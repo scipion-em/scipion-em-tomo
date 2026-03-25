55d0d3a09ded794c19dbef6e02be1b5cc2a6ea73
JorMaister <jorgejjs86@gmail.com>
2026-03-25 18:42:41 +0100

    Refactoring excl views

--- tomo/protocols/protocol_ts_exclude_views_filter.py
+++ tomo/protocols/protocol_ts_exclude_views_filter.py
@@ -24,23 +24,25 @@
 # *
 # **************************************************************************
 import logging
+import traceback
 from enum import Enum
 import time
-from typing import Union, List, Counter
+from typing import Union, Counter, Tuple
 
 from pyworkflow import BETA
 from pyworkflow.protocol.params import PointerParam, FloatParam, IntParam
 from pyworkflow.object import Set, Pointer
-from pyworkflow.utils import Message, makePath, cyanStr, redStr
+from pyworkflow.utils import cyanStr, Message, redStr
 from pwem.protocols import EMProtocol
+from pyworkflow.utils.retry_streaming import retry_on_sqlite_lock
 from tomo.objects import TiltSeries, TiltImage, SetOfTiltSeries
-from tomo.protocols import ProtTomoBase
 
 logger = logging.getLogger(__name__)
 
-#Form variables
-IN_TS_SET = 'inTsSet'
+EXCL_VIEWS_SUFFIX = '_exclViews'
 
+# Form variables
+IN_TS_SET = 'inTsSet'
 OUTPUT_TS_FAILED_NAME = "FailedTiltSeries"
 
 
@@ -48,11 +50,11 @@ class outputObjects(Enum):
     tiltSeries = SetOfTiltSeries
 
 
-class ProtExclViewFilter(EMProtocol, ProtTomoBase):
+class ProtExclViewFilter(EMProtocol):
     """
-    This protocol allows to filter a set of aligned tilt series according to a set of parameter as they are:\n
-     * Maximum allowed shift after the tilt series alignment\n
-     * Range of accumulated dose\n
+    This protocol allows to filter a set of aligned tilt series according to a set of parameter as they are:
+     * Maximum allowed shift after the tilt series alignment
+     * Range of accumulated dose
      * Range of tilt angle
      * Minimum number of views
     """
@@ -72,7 +74,7 @@ class ProtExclViewFilter(EMProtocol, ProtTomoBase):
 
     # -------------------------- DEFINE param functions -----------------------
     def _defineParams(self, form):
-        form.addSection('Input')
+        form.addSection(Message.LABEL_INPUT)
 
         form.addParam(IN_TS_SET,
                       PointerParam,
@@ -82,29 +84,29 @@ class ProtExclViewFilter(EMProtocol, ProtTomoBase):
                       help='Select several sets of tilt-series where to evaluate the consensus in their alignment. '
                            'Output set will bring the information from the first selected set.')
 
-        lineShift = form.addLine('Filter by shift (px)',
-                                help='This is the minimum/maximum shift allowed along the X or Y direction.'
-                                     ' A value of 0.1 means that a 10% of the dimensions of the tilt image '
-                                     'is allowed.')
-        lineShift.addParam('maxShiftX', FloatParam, default=0.1, label="Max shift in X")
-        lineShift.addParam('maxShiftY', FloatParam, default=0.1, label="Max shift in Y")
+        lineShift = form.addLine('Filter by max shift (px)',
+                                 help='This is the minimum/maximum shift allowed along the X or Y direction.'
+                                      ' A value of 0.1 means that a 10% of the dimensions of the tilt image '
+                                      'is allowed.')
+        lineShift.addParam('maxShiftX', FloatParam, default=0.1, label="X")
+        lineShift.addParam('maxShiftY', FloatParam, default=0.1, label="Y")
 
         lineTilt = form.addLine('Filter by tilt angle (deg)',
                                 help='This is the minimum/maximum tilt angle allowed.'
                                      'Only the tilt images with tilt angles in the range min<tilt<max are'
                                      'allowed.')
         lineTilt.addParam('mintilt', FloatParam, default=-30.0, label="Min")
-        lineTilt.addParam('maxtilt', FloatParam, default=30.0,  label="Max")
+        lineTilt.addParam('maxtilt', FloatParam, default=30.0, label="Max")
 
         lineDose = form.addLine('Filter by dose (e/A^2)',
                                 help='This is the minimum/maximum dose per tilt image.'
                                      'Only the tilt images with accumulated dose in this range will be kept.')
         lineDose.addParam('minDose', FloatParam, default=0.0, label="Min")
-        lineDose.addParam('maxDose', FloatParam, default=70.0,  label="Max")
+        lineDose.addParam('maxDose', FloatParam, default=70.0, label="Max")
 
         form.addParam('minViews',
                       IntParam,
-                      default = 30,
+                      default=30,
                       label="Min number of views",
                       help='Minimum number of views to include a tilt series.')
 
@@ -119,7 +121,7 @@ class ProtExclViewFilter(EMProtocol, ProtTomoBase):
             with self._lock:
                 inTsIds = set(inTsSet.getTSIds())
 
-            # In the if statement below, Counter is used because in the tsId comparison the order doesnt matter
+            # In the if statement below, Counter is used because in the tsId comparison the order doesn't matter
             # but duplicates do. With a direct comparison, the closing step may not be inserted because of the order:
             # ['ts_a', 'ts_b'] != ['ts_b', 'ts_a'], but they are the same with Counter.
             if not inTsSet.isStreamOpen() and Counter(self.itemTsIdReadList) == Counter(inTsIds):
@@ -134,8 +136,9 @@ class ProtExclViewFilter(EMProtocol, ProtTomoBase):
                                if (tsId := ts.getTsId()) in nonProcessedTsIds  # Only not processed tsIds
                                and ts.getSize() > 0}  # Avoid processing empty TS
             for tsId, ts in tsToProcessDict.items():
-                excId = self._insertFunctionStep(self.excludeViewFilteringStep, tsId,
-                                                     needsGPU=True)
+                excId = self._insertFunctionStep(self.excludeViewFilteringStep, ts,
+                                                 prerequisites=[],
+                                                 needsGPU=False)
                 closeSetStepDeps.append(excId)
                 logger.info(cyanStr(f"Steps created for tsId = {tsId}"))
                 self.itemTsIdReadList.append(tsId)
@@ -146,22 +149,24 @@ class ProtExclViewFilter(EMProtocol, ProtTomoBase):
                     inTsSet.loadAllProperties()  # refresh status for the streaming
 
     # --------------------------- STEPS functions ----------------------------
-    def excludeViewFilteringStep(self):
-        inputData = self._getInTsSet()
+    def excludeViewFilteringStep(self, ts: TiltSeries):
+        try:
+            self._registerOutput(ts)
+        except Exception as e:
+            logger.error(
+                redStr(f'tsId = {ts.getTsId()} -> Unable to register the output with exception {e}. Skipping... '))
+            logger.error(traceback.format_exc())
 
-        suffix = '_exclViews'
-        newSetTs = self._createSet(SetOfTiltSeries, 'tiltseries%s.sqlite', suffix)
-        newSetTs.copyInfo(inputData)
-        newSetTs.copyAttributes(inputData)
+    @retry_on_sqlite_lock(log=logger)
+    def _registerOutput(self, ts: TiltSeries):
+        with self._lock:
+            outTsSet = self.getOutputSetOfTS()
+            outTs = TiltSeries()
+            outTs.copyInfo(ts)
+            outTsSet.append(outTs)
 
-        xdim, ydim, ndim = inputData.getFirstItem().getDim()
-        xdimThreshold = self.maxShiftX.get()*xdim
-        ydimThreshold = self.maxShiftY.get()*ydim
-        minTiltThreshold = self.mintilt.get()
-        maxTiltThreshold = self.maxtilt.get()
-        minDoseThreshold = self.minDose.get()
-        maxDoseThreshold = self.maxDose.get()
-        #tsi = ts.getFirstEnabledITem()
+        xdimThreshold, ydimThreshold, minTiltThreshold, maxTiltThreshold, minDoseThreshold, maxDoseThreshold = (
+            self._getThresholds(ts))
 
         for ts in inputData:
 
@@ -180,22 +185,22 @@ class ProtExclViewFilter(EMProtocol, ProtTomoBase):
 
                 dose = ti.getAcquisition().getAccumDose()
 
-                if tiltAngle>maxTiltThreshold or tiltAngle<minTiltThreshold:
+                if tiltAngle > maxTiltThreshold or tiltAngle < minTiltThreshold:
                     newTi.setEnabled(False)
                 if ts.hasAlignment():
                     tm = ti.getTransform().getMatrix()
                     sx = tm[0, 2]
                     sy = tm[1, 2]
-                    if sx>xdimThreshold or sy>ydimThreshold:
+                    if sx > xdimThreshold or sy > ydimThreshold:
                         newTi.setEnabled(False)
 
-                if dose<minDoseThreshold or dose>maxDoseThreshold:
+                if dose < minDoseThreshold or dose > maxDoseThreshold:
                     newTi.setEnabled(False)
 
                 newTs.append(newTi)
                 counter = counter + 1
 
-            if counter>=self.minViews.get():
+            if counter >= self.minViews.get():
                 newTs.setDim(ts.getDim())
                 newTs.write()
                 newSetTs.update(ts)
@@ -225,10 +230,37 @@ class ProtExclViewFilter(EMProtocol, ProtTomoBase):
     def _getInTsSet(self, returnPointer: bool = False) -> Union[SetOfTiltSeries, Pointer]:
         inTsPointer = getattr(self, IN_TS_SET)
         return inTsPointer if returnPointer else inTsPointer.get()
+
+    def getOutputSetOfTS(self) -> SetOfTiltSeries:
+        attrName = self._possibleOutputs.tiltSeries.name
+        outputSet = getattr(self, attrName, None)
+        if outputSet:
+            outputSet.enableAppend()
+        else:
+            outputSet = SetOfTiltSeries.create(self._getPath(),
+                                               template='tiltseries',
+                                               suffix=EXCL_VIEWS_SUFFIX)
+            outputSet.copyInfo(self._getInTsSet())
+            outputSet.setStreamState(Set.STREAM_OPEN)
+            # Write set properties, otherwise it may expose the set (sqlite) without properties.
+            outputSet.write()
+            # Define outputs and relations
+            self._defineOutputs(**{attrName: outputSet})
+            self._defineSourceRelation(self._getInTsSet(returnPointer=True), outputSet)
+        return outputSet
+
+    def _getThresholds(self, ts: TiltSeries) -> Tuple[int, int, float, float, float, float]:
+        xdim, ydim, _ = ts.getFirstItem().getDim()
+        xdimThreshold = self.maxShiftX.get() * xdim
+        ydimThreshold = self.maxShiftY.get() * ydim
+        minTiltThreshold = self.mintilt.get()
+        maxTiltThreshold = self.maxtilt.get()
+        minDoseThreshold = self.minDose.get()
+        maxDoseThreshold = self.maxDose.get()
+        return xdimThreshold, ydimThreshold, minTiltThreshold, maxTiltThreshold, minDoseThreshold, maxDoseThreshold
+
     # --------------------------- INFO functions ----------------------------
     def _summary(self):
         summary = []
 
         return summary
-
-
